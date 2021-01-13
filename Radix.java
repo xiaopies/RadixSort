b0f2f3393baac331744acc03c665e58f06d66cb3
xiaopies <xiaoshenma2016@gmail.com>
2021-01-13 13:33:14 -0500

    edits

--- Radix.java
+++ Radix.java
@@ -15,4 +15,48 @@ public class Radix{
     String cheat = "" + n;
     return cheat.length();
   }
+  public static void merge( SortableLinkedList original, SortableLinkedList[]buckets){
+    for (int i =0; i<buckets.length; i++){
+      original.extend(buckets[i]);
+    }
+  }
+  public static void radixSortSimple(SortableLinkedList data){
+    SortableLinkedList[] now= new SortableLinkedList[10];
+    int counter = 0;
+    int biggest = 0;
+    int size = 0;
+    while (data.size() >0){
+      int here = data.remove(counter);
+      if (length(here)>size){
+        size = length(here);
+      }
+      int digit = nth(here, 0);
+      now[digit].add(here);
+      counter ++;
+    }
+    for (int i = 1; i< size; i++){
+      now = shush(now, i);
+    }
+  }
+  private static SortableLinkedList[] shush(SortableLinkedList[] in, int sig){
+    SortableLinkedList one = new SortableLinkedList();
+    SortableLinkedList[] two = new SortableLinkedList[10];
+    merge(one, in);
+    int counter =0;
+    int biggest = 0;
+    while (one.size()>0){
+      int here = one.remove(counter);
+      if (here< Math.pow(10, counter)){
+        int digit = 0;
+      }  else{
+        int digit = nth(here,sig);
+      }
+      two[digit].add(here);
+      counter ++;
+    }
+    return two;
+  }
+  public static void radixSort(SortableLinkedList data){}
+
+
 }
